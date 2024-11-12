f7d37448256c6c7905574e60cc306821175752ac
ed03e2 <ed03e2@gmail.com>
2024-11-11 18:15:14 -0600

    reseteo de contraseña completo

--- src/app/forgotpassword/forgotpassword.page.ts
+++ src/app/forgotpassword/forgotpassword.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../service/auth.service';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-forgotpassword',
   templateUrl: './forgotpassword.page.html',
   styleUrls: ['./forgotpassword.page.scss'],
 })
 export class ForgotpasswordPage implements OnInit {
-
-  constructor() { }
+email: string='';
+  constructor(
+    private Authservice: AuthService,
+    private toastCtrl: ToastController
+  ) { }
 
   ngOnInit() {
+  }
+  async ResetPasswordUser(){
+    try{
+      await this.Authservice.resetPassword(this.email);
+      this.showToast('Correo de recuperación enviado.');
+    } catch (error) {
+      this.showToast('Error al enviar el correo de recuperación.');
+      console.error(error);
+    }
+  }
+  async showToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
   constructor(
     public auth: AngularFireAuth
   ) { }
-
+  
   loginFireauth(value: any){
     return new Promise<any> ((resolve, reject)=>{
       this.auth.signInWithEmailAndPassword(value.email, value.password).then(
@@ -27,4 +27,22 @@ export class AuthService {
       )
     })
   }
-}
+
+  resetPassword(email: string){
+    return new Promise<void> ((resolve, reject)=>{
+      this.auth.sendPasswordResetEmail(email).then(
+        ()=> resolve(),
+        (error:any) => reject(error)
+      );
+    });
+  }
+  //Prueba de autenticacion con el telefono movil (En desarrollo)
+ // loginPhoneauth(value: any){
+   // return new Promise<any> ((resolve, reject)=>{
+     // this.auth.signInWithPhoneNumber(value.phone, value.password).then(
+        //(res: any) => resolve(res),
+        //(error: any) => reject(error)
+     // )
+   // })
+  }
+
